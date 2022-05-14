d26ab0cec43088650a5cf6efb2808d87ccbc840f
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-14 15:44:32 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('heading').innerHTML = localStorage['title'] || 'Scribble';
-document.getElementById('content').innerHTML = localStorage['text'] || 'This text is automatically saved every second';
-
-setInterval(function() {
-    // function that is saving the innerHTML of the div 
-    localStorage['title'] = document.getElementById('heading').innerHTML; // default text 
-    localStorage['text'] = document.getElementById('content').innerHTML; 
-}, 1000); 
