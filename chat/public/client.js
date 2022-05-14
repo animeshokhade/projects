16b009f8b7b6f8f992edae1a208e3b70bb5565c2
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-14 19:11:18 +0530

    Add files via upload

---
+++ chat/public/client.js
@@ -0,0 +1,18 @@
+// initialising socket, connection to server
+var socket = io.connect("http://localhost:3000")
+socket.on("connect", function(data) {
+    socket.emit("join", "Hello server from client"); 
+});
+
+// listener for 'thread' event, which updates messages 
+socket.on("thread", function(data) {
+    $("#thread").append("<li>" + data + "</li>"); 
+}); 
+
+// sends message to server, resets & prevents default form action
+$("form").submit(function() {
+    var message = $("#message").val();
+    socket.emit("messages", message); 
+    this.reset();
+    return false; 
+});
