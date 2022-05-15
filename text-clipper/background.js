6bea70f94f42b6aeece8fca5ac0ca7d27aad2d3f
Animesh Okhade <96253880+animeshokhade@users.noreply.github.com>
2022-05-15 22:53:37 +0530

    Add files via upload

---
+++ text-clipper/background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    let clippings = [];
+    chrome.storage.sync.get("list", function (result) {
+        if (request.selection && result.list) {
+            clippings = [...result.list, request.selection]; 
+        } else if (result.list) {
+            clippings = [...result.list]; 
+        } else {
+            clippings = [request.selection]; 
+        }
+        sendResponse({clips: clippings}); 
+        chrome.storage.sync.set({
+            list: clippings, 
+        });
+    });
+    return true 
+});
+
